771d9774c9e522696ed6bde5a63b494a8f5e7714
Richard Skogeby <richard.skogeby@outlook.com>
2019-07-11 17:54:49 +0200

    Implement debug mode and update status field

--- models/galil.py
+++ models/galil.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
 from models.verify_ip import is_valid_ipv4_address
 import gclib
+import random
 
 class MotionLink():
     ip_address = ''
+    debug = False
     def _execute(self,command):
-        try:
-            g = gclib.py()
-            if is_valid_ipv4_address(self.ip_address):
-                g.GOpen("".join([ip_address, ' --direct -s ALL']))
-            else:
-                raise AttributeError("Invalid IP address. ", v, " is not a valid IPv4 address.")
-            g.timeout = 5000
-            val = g.GCommand(command)
-            g.timeout = 5000
-            return val
-        except gclib.GclibError as e:
-            print('Unexpected GclibError:', e)
-        finally:
-            g.GClose()
+        if not self.debug:
+            try:
+                g = gclib.py()
+                if is_valid_ipv4_address(self.ip_address):
+                    g.GOpen("".join([ip_address, ' --direct -s ALL']))
+                else:
+                    raise AttributeError("Invalid IP address. ", v, " is not a valid IPv4 address.")
+                g.timeout = 5000
+                val = g.GCommand(command)
+                g.timeout = 5000
+                return val
+            except gclib.GclibError as e:
+                print('Unexpected GclibError:', e)
+            finally:
+                g.GClose()
+        else:
+            return str(random.randint(1,50000))
 
     def move(self, cmd):
         val = self._execute('merin={}'.format(cmd))
--- views/containergrid.py
+++ views/containergrid.py
@@ -1,8 +1,21 @@
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty, BooleanProperty
 from views.actionbuttons import ActionButtons
 from views.statusfields import StatusFields
 from views.labels import Labels
+from models.galil import MotionLink
+from kivy.clock import Clock
+
 
 class ContainerGrid(GridLayout):
+    rp = StringProperty('1000')
+
+
     def __init__(self, **kwargs):
         super(ContainerGrid, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update_status_fields, 0.1)
+        self.ml = MotionLink()
+        self.ml.debug = True
+
+    def update_status_fields(self, *args):
+        self.rp = self.ml.read_rp()
