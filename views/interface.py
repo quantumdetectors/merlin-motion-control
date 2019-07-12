0af3c27486c68f22733b9452fe22f4be6f8154d3
Richard Skogeby <richard.skogeby@outlook.com>
2019-07-12 17:35:15 +0200

    Move run method into init of app

--- views/interface.py
+++ views/interface.py
@@ -40,28 +40,5 @@ class MerlinMotionControlApp(App):
         return ContainerGrid(settings=self.settings)
 
 
-
-
-def run():
-    try:
-        settings = config.load_user_settings()
-        for k,v in settings.items():
-            print(k,v)
-    except AttributeError as e:
-        print(e.__class__, "".join(e.args))
-    except FileNotFoundError as e:
-        print(e.__class__, "".join(e.args))
-        settings = {
-            "ip_address":config.default_ip(),
-            "default_requested_position":config.default_requested_position(),
-            "speed":config.default_speed(),
-            "speed_out":config.default_speed_out()
-        }
-        print("Default settings are:")
-        for k,v in settings.items():
-            print(k,v)
-    MerlinMotionControlApp().run()
-
-
 if __name__ == '__main__':
     MerlinMotionControlApp().run()
