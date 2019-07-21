1a415fd819c9974ffc4ead99b00f24f09b83c4a3
Richard Skogeby <richard.skogeby@outlook.com>
2019-07-21 16:04:39 +0100

    Add imports for separating kv-lang into separate files.

--- views/interface.py
+++ views/interface.py
@@ -8,6 +8,19 @@ from kivy.app import App
 import kivy
 import config
 
+# Separate kv files
+from kivy.lang import Builder
+from views.modals.settings import settingswindow
+#from views.modals.settings.settingswindow import SettingsWindow
+from views.modals.info import infowindow
+#from views.modals.info.infowindow import InfoWindow
+#from views.screens.main.mainscreen import MainScreen
+#from views.screens.main.actionbuttons import ActionButtons
+#from views.screens.main.labels import Labels
+#from views.screens.main.statusfields import StatusFields
+from views.screens.main import mainscreen
+from views.static import styles
+
 kivy.require("1.11.1")
 
 
@@ -22,6 +35,10 @@ class MerlinMotionControlApp(App):
         impact the functionality of the Kivy framework.
         """
         super(MerlinMotionControlApp, self).__init__(**kwargs)
+        Builder.load_file(styles.__file__.split('.py')[0]+'.kv')
+        Builder.load_file(settingswindow.__file__.split('.py')[0]+'.kv')
+        Builder.load_file(infowindow.__file__.split('.py')[0]+'.kv')
+        Builder.load_file(mainscreen.__file__.split('.py')[0]+'.kv')
 
     def build(self):
         """Return a layout."""
