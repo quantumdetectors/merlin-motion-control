ce45198398fd5391a9711dbfdc06fc13918e46bd
Robert McLeod <robbmcleod@gmail.com>
2019-09-09 11:40:35 -0700

    Load resources for Kivy early

--- merlin_motion_control/__init__.py
+++ merlin_motion_control/__init__.py
@@ -2,6 +2,10 @@
 """
 Quantum Detectors Merlin Motion Control
 """
+# Import the resources (icons, etc.) for Kivy early:
+import os.path as path
+from kivy.resources import resource_add_path
+resource_add_path(path.dirname(__file__))
 
 def main():
     from .views.interface import MerlinMotionControlApp
