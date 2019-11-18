98190e90897bf164dd9201e49ca06e18be87d728
Richard Skogeby <richard.skogeby@outlook.com>
2019-11-18 12:00:17 +0000

    Fix CETA interlock

---
+++ merlin_motion_control/Merlin_Motion_Control.py
@@ -0,0 +1,10 @@
+#!/Users/richard/anaconda3/bin/python3
+
+"""Launch app using Anaconda 3."""
+
+from views.interface import MerlinMotionControlApp
+import config
+
+
+if __name__ == "__main__":
+    MerlinMotionControlApp().run()
